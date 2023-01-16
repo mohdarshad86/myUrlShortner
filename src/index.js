757337db0642ff6107e923c7e0c7b920c1c1f58d
Mohd Arshad <mohdarshad8668@gmail.com>
2023-01-16 19:18:10 +0530

    added Basic structure - npm init, Schema, index, route, controller

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const route = require("./routes/route");
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.json());
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(
+    "mongodb+srv://mohdarshad86:Arshad86@cluster0.r4p7rwf.mongodb.net/group18Database",
+    { useNewUrlParser: true }
+  )
+  .then(() => {
+    console.log("BatsyDB is connected");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+app.use("/", route);
+
+app.listen(port || 3000, () => {
+  console.log(`Server is running on port ${port || 3000}`);
+});
