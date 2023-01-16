5253a3bde4039197d1f5baf2df344983288bd1bc
Mohd Arshad <mohdarshad8668@gmail.com>
2023-01-16 23:59:54 +0530

    create and get apis done

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -1,56 +1,66 @@
 const urlModel = require("../models/urlModel");
-const isValidUrl=require("valid-url");
+const isValidUrl = require("valid-url");
+const shortId = require("shortid");
 
 exports.createUrl = async (req, res) => {
-   try{ let data=req.body;
-    let newObj={};
-    if(!data.longUrl){
-        return res.status(400).send({status:false,msg:"Url is mandatory..."});
+  try {
+    let longUrl = req.body.longUrl;
+
+    let newObj = {};
+
+    if (!longUrl) {
+      return res
+        .status(400)
+        .send({ status: false, msg: "Url is mandatory..." });
     }
-    let urlCode=shortid.generate(data.longUrl);
-    let shortUrl= "http://localhost:3000/"+urlCode;
-    newObj.urlCode=urlCode;
-    newObj.longUrl=data.longUrl;
-    newObj.shortUrl=shortUrl;
 
-    const a= await urlModel.create(newObj);
-    return res.status(201).send({status:true,data:newObj});
-}catch(err){
-    return res.status(500).send({status:false,Error:err.message});
-}
+    if (!isValidUrl.isUri(longUrl)) {
+      return res.status(400).send({ status: false, msg: "Url is not valid" });
+    }
 
+    let getUrl = await urlModel.findOne({ longUrl: longUrl });
+    
+    if (getUrl) {
+      return res
+        .status(400)
+        .send({ status: false, msg: "ShortUrl Already generated" });
+    }
+
+    let urlCode = shortId.generate(longUrl);
+
+    let shortUrl = "http://localhost:3000/" + urlCode;
+
+    newObj.urlCode = urlCode;
+    newObj.longUrl = longUrl;
+    newObj.shortUrl = shortUrl;
+
+    const a = await urlModel.create(newObj);
+    return res.status(201).send({ status: true, data: newObj });
+  } catch (err) {
+    return res.status(500).send({ status: false, Error: err.message });
+  }
 };
 
-
 exports.getUrl = async (req, res) => {
+  try {
+    let urlCode = req.params.urlCode;
 
-try{
-let urlCode= req.params.urlCode;
-if(!urlCode) return res.status(400).send({status:false,message:"Put urlCode first"})
-if(typeof urlCode!=String) return res.status(400).send({status:false,message:"urlCode is not Valid"})
+    if (!urlCode)
+      return res
+        .status(400)
+        .send({ status: false, message: "Put urlCode first" });
 
-let urlDetails= await urlModel.findOne({urlCode:urlCode});
+    urlCode = urlCode.toLowerCase();
 
-if(!urlDetails) return res.status(404).send({status:false,message:"url Not Found"})
+    let urlDetails = await urlModel.findOne({ urlCode: urlCode });
 
-let longUrl=urlDetails.longUrl;
+    if (!urlDetails)
+      return res.status(404).send({ status: false, message: "url Not Found" });
 
-return res.status(302).redirect({url:longUrl})
+    let longUrl = urlDetails.longUrl;
 
-}
-
-catch(error){
-
-return res.status(500).send({status:false,error:error.message})
-
-
-}
-
-
-
-
-
-
-
-
+    return res.status(302).redirect(longUrl);
+  } catch (error) {
+    return res.status(500).send({ status: false, error: error.message });
+  }
 };
